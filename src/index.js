11b5df24d17d71668af4cba7c38d0b29c547d8d6
Chris baird <chrisbaird05@icloud.com>
2020-08-07 14:43:52 -0700

    dfgdfg

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import * as serviceWorker from "./serviceWorker";
 import { Auth0Provider } from "@auth0/auth0-react";
 import config from "./auth_config.json";
 import history from "./utils/history";
-console.log(config)
+console.log(config);
+
 const onRedirectCallback = (appState) => {
   history.push(
     appState && appState.returnTo
@@ -17,7 +18,7 @@ const onRedirectCallback = (appState) => {
 
 ReactDOM.render(
   <Auth0Provider
-    domain={config.reactDomain}
+    domain={config.domain}
     clientId={config.clientId}
     audience={config.audience}
     redirectUri={window.location.origin}
