8858d1e8d49382ec272e96ce029228570f2a4789
Chris baird <chrisbaird05@icloud.com>
2020-08-07 13:55:03 -0700

    served up public folder in api server

--- api-server.js
+++ api-server.js
@@ -5,10 +5,7 @@ const helmet = require("helmet");
 const jwt = require("express-jwt");
 const jwksRsa = require("jwks-rsa");
 const authConfig = require("./src/auth_config.js");
-// Testing Start
-console.log(authConfig);
-
-// Testing End
+const { join } = require("path");
 const app = express();
 
 const port = process.env.PORT || 3001;
@@ -24,6 +21,7 @@ if (!authConfig.domain || !authConfig.audience) {
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors({ origin: appOrigin }));
+app.use(express.static(join(__dirname, "build")));
 
 const checkJwt = jwt({
   secret: jwksRsa.expressJwtSecret({
