bbb22ad96be1771043947121be84c63098ddafad
Chris baird <chrisbaird05@icloud.com>
2020-08-07 20:44:46 -0700

    labeled api server with comments

--- api-server.js
+++ api-server.js
@@ -6,22 +6,25 @@ const jwt = require("express-jwt");
 const jwksRsa = require("jwks-rsa");
 const authConfig = require("./src/auth_config.json");
 const { join } = require("path");
-
 const app = express();
-
 const port = process.env.PORT || 3001;
 const appPort = process.env.SERVER_PORT || 3000;
 const appOrigin = authConfig.appOrigin || `http://localhost:${appPort}`;
 
+// Checks for auth config
 if (!authConfig.domain || !authConfig.audience) {
   throw new Error(
     "Please make sure that auth_config.json is in place and populated"
   );
 }
 
+// HTTP logger middleware
 app.use(morgan("dev"));
+// HTTP header middleware
 app.use(helmet());
+// Cross origin middleware
 app.use(cors({ origin: appOrigin }));
+// Serves up react app
 app.use(express.static(join(__dirname, "build")));
 
 const checkJwt = jwt({
@@ -31,7 +34,6 @@ const checkJwt = jwt({
     jwksRequestsPerMinute: 5,
     jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
   }),
-
   aud: authConfig.audience,
   issuer: `https://${authConfig.domain}/`,
   algorithm: ["RS256"]
@@ -43,4 +45,5 @@ app.get("/api/external", checkJwt, (req, res) => {
   });
 });
 
+// Sets server to listen on given port
 app.listen(port, () => console.log(`API Server listening on port ${port}`));
