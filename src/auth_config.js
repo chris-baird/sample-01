9618b606e9c801b92336442e9644d0f0f76cb60a
Chris baird <chrisbaird05@icloud.com>
2020-08-08 11:12:23 -0700

    removed test file

--- src/auth_config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "domain": process.env.DOMAIN,
-    "clientId": process.env.CLIENTID,
-    "audience": process.env.AUDIENCE,
-    "reactDomain": process.env.REACT_APP_DOMAIN,
-    "reactClientId": process.env.REACT_APP_CLIENTID,
-    "reactAudience": process.env.REACT_APP_AUDIENCE,
-}
